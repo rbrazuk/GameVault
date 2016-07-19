71385659542c31b48645e11b9c215977ee214c11
Ross Brazuk <rossbrazuk1@Ross-Brazuks-Macbook.local>
2016-07-19 10:34:58 -0400

    initial commit

---
+++ app/src/main/java/com/example/rbrazuk/gamevault/models/Game.java
@@ -0,0 +1,9 @@
+package com.example.rbrazuk.gamevault.models;
+
+public class Game {
+
+    private String title;
+    private String system;
+    private String genre;
+    private int yearReleased;
+}
